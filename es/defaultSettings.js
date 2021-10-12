2f22853ed27083bee63616b5ca04c3e5cfbecf07
yuan.yan <yuan.yan@yuanyandeMacBook-Pro.local>
2021-10-12 16:27:36 +0800

    created

---
+++ es/defaultSettings.js
@@ -0,0 +1,13 @@
+var defaultSettings = {
+  navTheme: 'dark',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: false,
+  headerHeight: 48,
+  title: 'Ant Design Pro',
+  iconfontUrl: '',
+  primaryColor: 'daybreak',
+  splitMenus: false
+};
+export default defaultSettings;
